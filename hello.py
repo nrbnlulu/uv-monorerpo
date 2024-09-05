dbb9aaf50a40c10caa8201e9e045b32b48d1872e
nir <nrbnlulu@gmail.com>
2024-09-05 08:27:21 +0300

    fc

---
+++ hello.py
@@ -0,0 +1,12 @@
+def main():
+    def this_fails():
+        from sdk.amaizing import so_amaizing
+        print(so_amaizing())
+    def this_works():
+        from libs.sdk.src.sdk.amaizing import so_amaizing
+        print(so_amaizing())
+    this_works()
+    this_fails()
+
+if __name__ == "__main__":
+    main()
---
+++ libs/sdk/src/sdk/amaizing.py
@@ -0,0 +1,2 @@
+def so_amaizing() -> str:
+    return "I'm so amazing!"
